82b26b6ced3d22e5416c49ca46721f8027c5ae18
Nishant Kumar <93445529+NishantMsingh@users.noreply.github.com>
2023-01-28 23:02:58 +0530

    changed

---
+++ script.js
@@ -0,0 +1,7 @@
+var list=document.getElementsByTagName("li");
+list[3].style.backgroundColor="green";
+
+for (var i = 0; i < list.length; i++) {
+    // Change the font weight to bold
+    list[i].style.fontWeight = "bold";
+  }
