ce9dc3828cf2bd8773a7a09070ae4a65c55c3763
Nishant Kumar <93445529+NishantMsingh@users.noreply.github.com>
2023-01-28 23:21:46 +0530

    all done by query selector all

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var list=document.getElementsByTagName("li");
-list[3].style.backgroundColor="green";
+// var list=document.getElementsByTagName("li");
+// list[3].style.backgroundColor="green";
 
-for (var i = 0; i < list.length; i++) {
-    // Change the font weight to bold
-    list[i].style.fontWeight = "bold";
-    aegeagqrggr
-  }
+// var vis=document.querySelector(".list-group-item:nth-child(3)");
+// var col=document.querySelector(".list-group-item:nth-child(2)");
+// col.style.color="green";
+// vis.style.display="none";
+
+// for (var i = 0; i < list.length; i++) {
+//     // Change the font weight to bold
+//     list[i].style.fontWeight = "bold";
+//   }
+
+// Using Query Selector All
+var list=document.querySelectorAll("li:nth-child(odd)");
+for(i=0;i<list.length;i++)
+{
+ list[i].style.backgroundColor="green";
+}
+var col=document.querySelector("li:nth-child(2)");
+col.style.color="green";
