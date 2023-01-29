237d13fc9a8a6d9e7e67cf2aeb6642907561c70d
Nishant Kumar <93445529+NishantMsingh@users.noreply.github.com>
2023-01-29 20:14:08 +0530

    used all siblings and child and parents

--- script.js
+++ script.js
@@ -12,10 +12,59 @@
 //   }
 
 // Using Query Selector All
-var list=document.querySelectorAll("li:nth-child(odd)");
-for(i=0;i<list.length;i++)
-{
- list[i].style.backgroundColor="green";
-}
-var col=document.querySelector("li:nth-child(2)");
-col.style.color="green";
+// var list=document.querySelectorAll("li:nth-child(odd)");
+// for(i=0;i<list.length;i++)
+// {
+//  list[i].style.backgroundColor="green";
+// }
+// var col=document.querySelector("li:nth-child(2)");
+// col.style.color="green";
+
+// tRAVERSING THE DOM  
+var itemlist=document.querySelector("#items");
+//Parent node property
+// console.log(itemlist.parentNode);
+// console.log(itemlist.parentNode.parentNode);
+// console.log(itemlist.parentNode.parentNode.parentNode);
+// itemlist.parentElement.style.backgroundColor="#fcfcfc";
+
+// Parent Element
+//  console.log(itemlist.parentElement);
+// console.log(itemlist.parentNode.parentElement);
+// console.log(itemlist.parentNode.parentNode.parentElement);
+
+// Child Node 
+// console.log(itemlist.childNodes);
+// console.log(itemlist.children[2].style.color="white");
+// console.log(itemlist.firstChild);
+// console.log(itemlist.lastChild);
+// console.log(itemlist.lastElementChild);
+// itemlist.lastElementChild.innerHTML="Nishant";
+
+
+// Siblings 
+// console.log(itemlist.nextElementSibling);
+// console.log(itemlist.nextSibling);
+
+// // Previous sibling 
+// console.log(itemlist.previousSibling); 
+// console.log(itemlist.previousElementSibling); 
+
+// Create Element and insert  
+
+var ndiv=document.createElement("div");
+ndiv.className="Hello";
+ndiv.id="hello1";
+ndiv.setAttribute("title","Hello div");
+
+
+// Creating text node 
+var newdivtxt=document.createTextNode("Hello");
+ndiv.appendChild(newdivtxt);
+console.log(ndiv);
+
+// Insert into HTML 
+var container=document.querySelector("header .container");
+var h1=document.querySelector("header h1");
+container.insertBefore(ndiv,h1);
+ndiv.style.fontSize="30px";
