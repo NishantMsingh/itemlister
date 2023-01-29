bc5b30e1aaa15f24f84ea0dbc45b1559c60e85d0
Nishant Kumar <93445529+NishantMsingh@users.noreply.github.com>
2023-01-29 23:11:01 +0530

    final commit 1.0

--- script.js
+++ script.js
@@ -1,68 +1,151 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
-itemList.style.cursor="pointer";
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+// var list=document.getElementsByTagName("li");
+// list[3].style.backgroundColor="green";
 
-// Add item
-function addItem(e){
+// var vis=document.querySelector(".list-group-item:nth-child(3)");
+// var col=document.querySelector(".list-group-item:nth-child(2)");
+// col.style.color="green";
+// vis.style.display="none";
+
+// for (var i = 0; i < list.length; i++) {
+//     // Change the font weight to bold
+//     list[i].style.fontWeight = "bold";
+//   }
+
+// Using Query Selector All
+// var list=document.querySelectorAll("li:nth-child(odd)");
+// for(i=0;i<list.length;i++)
+// {
+//  list[i].style.backgroundColor="green";
+// }
+// var col=document.querySelector("li:nth-child(2)");
+// col.style.color="green";
+
+// tRAVERSING THE DOM  
+// var itemlist=document.querySelector("#items");
+//Parent node property
+// console.log(itemlist.parentNode);
+// console.log(itemlist.parentNode.parentNode);
+// console.log(itemlist.parentNode.parentNode.parentNode);
+// itemlist.parentElement.style.backgroundColor="#fcfcfc";
+
+// Parent Element
+//  console.log(itemlist.parentElement);
+// console.log(itemlist.parentNode.parentElement);
+// console.log(itemlist.parentNode.parentNode.parentElement);
+
+// Child Node 
+// console.log(itemlist.childNodes);
+// console.log(itemlist.children[2].style.color="white");
+// console.log(itemlist.firstChild);
+// console.log(itemlist.lastChild);
+// console.log(itemlist.lastElementChild);
+// itemlist.lastElementChild.innerHTML="Nishant";
+
+
+// Siblings 
+// console.log(itemlist.nextElementSibling);
+// console.log(itemlist.nextSibling);
+
+// // Previous sibling 
+// console.log(itemlist.previousSibling); 
+// console.log(itemlist.previousElementSibling); 
+
+// Create Element and insert  
+
+// var ndiv=document.createElement("div");
+// ndiv.className="Hello";
+// ndiv.id="hello1";
+// ndiv.setAttribute("title","Hello div");
+
+
+// // Creating text node 
+// var newdivtxt=document.createTextNode("Hello");
+// ndiv.appendChild(newdivtxt);
+// console.log(ndiv);
+
+// // Insert into HTML 
+// var container=document.querySelector("header .container");
+// var h1=document.querySelector("header h1");
+// container.insertBefore(ndiv,h1);
+// ndiv.style.fontSize="30px";
+
+
+// Working on the application
+var form=document.getElementById("addForm");
+
+// Filter 
+var filter=document.getElementById("filter");
+
+
+var itemList=document.getElementById("items");
+// Delete Event 
+itemList.addEventListener('click',deleList);
+// Submit and Adding list Event 
+form.addEventListener('submit',addItem);
+
+
+// Filter Event 
+filter.addEventListener('keyup',filteritems)
+// Add item 
+function addItem(e)
+{
   e.preventDefault();
+  var newItem=document.getElementById("item").value;
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
+// Saving to the text 
+var text = document.getElementById("item").value;
+    var blob = new Blob([text], {type: "text/plain"});
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+//   Creating List
+var li=document.createElement("li");
+li.className="list-group-item";
+li.appendChild(document.createTextNode(newItem));
+// Creating delete button
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-  deleteBtn.style.cursor="pointer";
+var del=document.createElement("button");
+// Add class 
+del.className="btn btn-danger btn-sm float-right delete";
+// Append the text node 
+del.appendChild(document.createTextNode("X"));
+li.appendChild(del);
+var txt=li.innerText;
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
+    itemList.appendChild(li);
+document.getElementById("item").innerHTML="";
 }
 
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
+
+// Remove Item 
+function deleList(e)
+{
+  if(e.target.classList.contains('delete'))
+  {
+   if(confirm("Are you sure?"))
+   {
+    var li1=e.target.parentElement;
+    itemList.removeChild(li1);
+   }
   }
 }
 
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
+function filteritems(e)
+{
+    //convert 
+    var text1=e.target.value.toLowerCase();
+   var items=itemList.getElementsByTagName('li');
+//    Convert it to array 
+Array.from(items).forEach(function(item){
+var itemName=item.firstChild.textContent;
+if(itemName.toLocaleLowerCase().indexOf(text1)!=-1)
+{
+item.style.display="block";
+}
+else{
+    item.style.display="none";
+}
+});
+
 }
